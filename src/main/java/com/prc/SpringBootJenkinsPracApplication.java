a3fc05298455100672ac60aedf609c0ca37745e9
NikhilPandey1234 <nikhilpanday46@gmail.com>
2023-10-19 13:43:36 +0530

    First Commit

---
+++ src/main/java/com/prc/SpringBootJenkinsPracApplication.java
@@ -0,0 +1,13 @@
+package com.prc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prc/SpringBootJenkinsPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
